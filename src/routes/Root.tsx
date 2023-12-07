87ae211c2ba0b27d0078e1ad1c502b51a94348e5
1z3r091 <1z3r091@gmail.com>
2023-12-07 17:56:25 +0900

    feat/initial mindmap

--- src/routes/Root.tsx
+++ src/routes/Root.tsx
@@ -1,3 +1,55 @@
+import { useRef, useEffect } from "react";
+import cytoscape from "cytoscape";
+
 export default function Root() {
-  return <div></div>;
+  const cyRef = useRef();
+
+  useEffect(() => {
+    const cy = cytoscape({
+      container: cyRef.current,
+      elements: [
+        {
+          data: { id: "node1", label: "Node 1" },
+          position: { x: 100, y: 100 },
+        },
+        {
+          data: { id: "node2", label: "Node 2" },
+          position: { x: 200, y: 200 },
+        },
+        { data: { id: "edge1", source: "node1", target: "node2" } },
+      ],
+      style: [
+        {
+          selector: "node",
+          style: {
+            "background-color": "#66ccff",
+            label: "data(label)",
+          },
+        },
+        {
+          selector: "edge",
+          style: {
+            "line-color": "#ccc",
+          },
+        },
+      ],
+    });
+    return () => {
+      cy.destroy();
+    };
+  }, []);
+  return (
+    <main className="main">
+      <p>일단 리스트로 보여주고 나중에 구체화</p>
+      <ul className="brainwords">
+        <li>33% - Frontend</li>
+        <ul>
+          <li>11% - React</li>
+        </ul>
+        <li>22% - Learning</li>
+        <li>11% - German</li>
+      </ul>
+      <div ref={cyRef} style={{ width: "100%", height: "400px" }}></div>
+    </main>
+  );
 }
