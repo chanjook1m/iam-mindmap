1f8cb34de5109f302c9d5e45d9d90fa3e1c80989
1z3r091 <1z3r091@gmail.com>
2023-12-11 17:22:45 +0900

    feat/header navlink

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -1,11 +1,19 @@
+import { NavLink } from "react-router-dom";
 import "./header.styles.css";
 export default function Header() {
+  const data = [
+    { id: 1, path: "/", title: "Calendar" },
+    { id: 2, path: "/view2", title: "View2" },
+  ];
   return (
     <nav className="header">
       <span> Logo</span>
       <ul className="header__navbar">
-        <li>Brain</li>
-        <li>Footprint</li>
+        {data.map((ele) => (
+          <li key={ele.id}>
+            <NavLink to={ele.path}>{ele.title}</NavLink>
+          </li>
+        ))}
       </ul>
     </nav>
   );
