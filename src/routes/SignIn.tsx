53458e1b705f146e652413d524fefce6c6b7bdf4
1z3r091 <1z3r091@gmail.com>
2023-12-15 23:14:22 +0900

    chore/set redirect url after signin

--- src/routes/SignIn.tsx
+++ src/routes/SignIn.tsx
@@ -29,7 +29,7 @@ export default function SignIn() {
             supabase.auth.signInWithOAuth({
               provider: "google",
               options: {
-                // redirectTo: window.location.origin + "/stats",
+                redirectTo: window.location.origin + "/stats",
                 queryParams: { prompt: "select_account" },
               },
             });
